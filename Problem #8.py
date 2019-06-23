14f5d75503b84891eccec9c70eea3c5a226b3279
ash4995 <yashshah_95@yahoo.co.in>
2019-06-23 14:54:30 -0700

    Had to refer StackOverflow but got to learn itertools combinations.

--- Problem #8.py
+++ Problem #8.py
@@ -1,12 +1,24 @@
 # Write a method to return all subsets of a set.
 #
-from itertools import chain, combinations
 
 #result = set()
 
-def subsets(set):
-   s = list(set)
-   return chain.from_iterable(combinations(s,r) for r in range(len(s)+1))
+# def subsets(set):
+# #    s = list(set)
+# #    return chain.from_iterable(combinations(s,r) for r in range(len(s)+1))
+# #
+# #
+# # print (list(subsets({1,2,3,4})))
+
+# def powerset(s):
+#    x = len(s)
+#    masks = [1 << i for i in range(x)]
+
+# for i in range(1 << x):
+#    yield [ss for mask, ss in zip(masks, s) if i & mask]
 
 
-print (list(subsets({1,2,3,4})))
+# print (list(powerset({1,2,3,4})))
+masks = (1 << 4)
+
+print(masks)
