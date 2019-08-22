ef1f690016a3c31076ed1111a67a1a340e83e112
ash4995 <yash95shah@gmail.com>
2019-08-22 13:21:38 -0700

    Added another way of approaching the perms problem - Use extra space

--- applyperm.py
+++ applyperm.py
@@ -1,7 +1,7 @@
 def apply_perm(Arr, Per):
     if Arr is None or len(Arr) == 0 or len(Arr) != len(Per):
         return 
-    for idx , elem in enumerate(Arr):
+    for idx in range(len(Arr)):
         if idx == Per[idx]:
             pass
         else:
@@ -9,12 +9,20 @@ def apply_perm(Arr, Per):
             Per = swap_elems(Per, idx, Per[idx] )
     return Arr
 
+
+def bad_sol(Arr, Perm):
+    ext_arr = [0]*len(Arr)
+    for idx, val in enumerate(Perm):
+        ext_arr[val] = Arr[idx]
+    return ext_arr
+
         
 def swap_elems(any_arr, i,j):
     any_arr[i],any_arr[j] = any_arr[j],any_arr[i]
     return any_arr
 
 
-Arra  = ['a','b','c','d']    
-Permu = [2,0,1,3]
-print (apply_perm(Arra,Permu))
+Arra  = ['a','b','c','d','e','f']    
+Permu = [1,0,5,4,3,2]
+# b a f e d c
+print (bad_sol(Arra,Permu))
